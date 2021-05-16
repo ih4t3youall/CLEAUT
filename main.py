1c5c120bdd8fdef9efd7fd02b2b92b6798d341b6
ih4t3youall <ih4t3youall>
2021-05-16 16:15:18 -0300

    gamma

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests;
+import time
+import threading
 import sys
 from PyQt5 import QtWidgets,QtGui
 from ui.cleautui import Ui_MainWindow  # importing our generated file
@@ -15,11 +17,28 @@ class mywindow(QtWidgets.QMainWindow):
         SXP="SXPUSDT"
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        values = self.get_prices()
-        self.ui.labela.setText('ETHUSDT')
-        self.ui.labelb.setText('SXPUSDT')
-        self.ui.valuea.setText(values[ETH])
-        self.ui.valueb.setText(values[SXP])
+        self.ui.labela.setText(ETH)
+        self.ui.labelb.setText(SXP)
+
+
+    def start(self):
+        ETH="ETHUSDT"
+        SXP="SXPUSDT"
+        while (True):
+            time.sleep(1)
+            values = self.get_prices()
+            self.ui.valuea.setText(values[ETH])
+            self.ui.valueb.setText(values[SXP])
+            self.ui.result.setText('eth/sxp: '+str(float(values[ETH])/float(values[SXP])))
+
+    def set_values(self, values):
+            ETH = "ETHUSDT"
+            SXP = "SXPUSDT"
+            self.ui.valuea.setText(values[ETH])
+            self.ui.valueb.setText(values[SXP])
+            print("hola")
+            print((float(float(values[ETH])/float(values[SXP]))))
+            self.ui.result.setText('eth/sxp: '+str(float(float(values[ETH])/float(values[SXP]))))
 
         
     def get_prices(self):
@@ -32,4 +51,12 @@ class mywindow(QtWidgets.QMainWindow):
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
+#thread
+x = threading.Thread(target=application.start, args=())
+x.start()
+#end thread
 sys.exit(app.exec())
+
+
+print('finish')
+
