92afa0bf40e9a313129c7adf351edada4e77ba8a
ih4t3youall <ih4t3youall>
2021-05-16 15:08:12 -0300

    working

--- main.py
+++ main.py
@@ -11,27 +11,15 @@ class mywindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(mywindow, self).__init__()
+        ETH="ETHUSDT"
+        SXP="SXPUSDT"
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         values = self.get_prices()
         self.ui.labela.setText('ETHUSDT')
         self.ui.labelb.setText('SXPUSDT')
-        self.ui.valuea.setText(values['ETHUSDT'])
-        self.ui.valueb.setText(values['SXPUSDT'])
-
-        #test
-        self.graphWidget = pg.PlotWidget()
-        #self.setCentralWidget(self.graphWidget)
-        #self.ui.graphicsView.addItem(self.graphWidget)
-        #self.ui.graphicsView
-        #self.ui.scrollArea
-
-        #hour = [1,2,3,4,5,6,7,8,9,10]
-        #temperature = [30,32,34,32,33,31,29,32,35,45]
-
-        # plot data: x, y values
-        #self.graphWidget.plot(hour, temperature)
-        #end test
+        self.ui.valuea.setText(values[ETH])
+        self.ui.valueb.setText(values[SXP])
 
         
     def get_prices(self):
